61883f9e34da99b14b2e8c2add614eaa1b0c5054
itsara <khajer@gmail.com>
2024-08-07 15:59:20 +0700

    rename file

--- programs/money_expense/src/lib.rs
+++ programs/money_expense/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::prelude::*;
 
-declare_id!("GLTLDPkRb1ycFexozV1KbAR8hFPxUQ4K2rRWhC4AuweF");
+declare_id!("8QYJS7akSyHJEoAg6LZ5oVCMxSGr6Dz9qFciwLz2mQRC");
 
 #[program]
-pub mod xxx {
+pub mod money_expense {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
--- tests/money_expense.ts
+++ tests/money_expense.ts
@@ -1,12 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Xxx } from "../target/types/xxx";
+import { MoneyExpense } from "../target/types/money_expense";
 
-describe("xxx", () => {
+describe("money_expense", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Xxx as Program<Xxx>;
+  const program = anchor.workspace.MoneyExpense as Program<MoneyExpense>;
 
   it("Is initialized!", async () => {
     // Add your test here.
